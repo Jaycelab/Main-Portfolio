57d0f5d95307a80416d66be5d99b0e9101dc6e8c
Jason Alvarado <jayxalvarado@gmail.com>
2024-06-17 07:34:56 -0700

    nav .jsx cleanup

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 
 const links = [
   {
-    name: "home",  
+    name: "home",
     path: "/",
   },
   {
@@ -23,11 +23,19 @@ const links = [
 ];
 
 const Nav = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <nav className="flex gap-8">
       {links.map((link, index) => {
         return (
-          <Link href={link.path} key={index}>
+          <Link
+            href={link.path}
+            key={index}
+            className={`${
+              link.path === pathname && "text-accent border-b-2 border-accent"
+            }`}
+          >
             {link.name}
           </Link>
         );
