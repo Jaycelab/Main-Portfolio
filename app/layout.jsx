efaf72f8ad72d4abb002b1c68a0bf713fc322f92
Jason Alvarado <jayxalvarado@gmail.com>
2024-06-24 01:47:48 -0700

    setup analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,7 @@ import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const jetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
 
         <PageTransition> {children}</PageTransition>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
