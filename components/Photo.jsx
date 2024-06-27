75c8fa92ab2b27085cfbf8ed91a1fbe99a3be9aa
Jason Alvarado <jayxalvarado@gmail.com>
2024-06-27 15:31:38 -0700

    cleanup

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -10,7 +10,7 @@ const Photo = () => {
         initial={{ opacity: 0 }}
         animate={{
           opacity: 1,
-          transition: { delay: 1.1, duration: 0.4, ease: "easeIn" },
+          transition: { delay: 1.1, duration: 0.3, ease: "easeIn" },
         }}
       >
         {/*exit*/}
@@ -26,19 +26,20 @@ const Photo = () => {
           alternative class object-contain*/}
           <Image
             priority
-            src="/images/profile.png"
+            src="/images/profile-red.png"
             className="rounded-full"
             quality={100}
             responsive="true"
-            width="295"
-            height="295"
+            width="298"
+            zIndex="-10"
+            height="298"
             alt=""
           />
         </motion.div>
 
         {/*circle absolute top-0 left-0 w-full h-full xl:w-[498px] xl:h-[498px]*/}
         <motion.svg
-          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          className="w-[298px] xl:w-[506px] h-[298px] xl:h-[506px]"
           fill="transparent"
           viewBow="0 0 506 506"
           xmlns="http://www.w3.org/2000/svg"
@@ -47,19 +48,19 @@ const Photo = () => {
             cy="153"
             r="150" mobile*/}
           <motion.circle
-            cx="150"
-            cy="150"
+            cx="153"
+            cy="153"
             r="150"
-            stroke="#A91717"
-            strokeWidth="4"
+            stroke="#a91717"
+            strokeWidth="2.7"
             strokeLinecap="round"
             strokeLinejoin="round"
             initial={{ strokeDasharray: "24 10 0 0" }}
             animate={{
               strokeDasharray: ["15 120 25 25 ", "16 25 92 72", "4 250 22 22"],
               transition: {
-                delay: 1.5,
-                duration: 2.5,
+                delay: 0.3,
+                duration: 2.9,
                 ease: "easeInOut",
                 repeat: Infinity,
                 repeatType: "reverse",
