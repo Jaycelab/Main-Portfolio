777f52fd24fe2739bbadb3daf07a7b53d99a325a
Jason Alvarado <jayxalvarado@gmail.com>
2024-06-27 00:29:45 -0700

    Worked on Circle Animation Also...debugged circle animation

--- app/page.jsx
+++ app/page.jsx
@@ -42,7 +42,7 @@ const Home = () => {
                 {/*buttons, links*/}
                 <Links
                   containerStyles="flex gap-6"
-                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-al duration-800 "
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500 "
                 />
               </div>
             </div>
--- components/Photo.jsx
+++ components/Photo.jsx
@@ -10,7 +10,7 @@ const Photo = () => {
         initial={{ opacity: 0 }}
         animate={{
           opacity: 1,
-          transition: { delay: 1.5, duration: 0.4, ease: "easeIn" },
+          transition: { delay: 1.1, duration: 0.4, ease: "easeIn" },
         }}
       >
         {/*exit*/}
@@ -18,11 +18,12 @@ const Photo = () => {
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
-            transition: { delay: 2.5, duration: 0.4, ease: "easeOut" },
+            transition: { delay: 3, duration: 0.4, ease: "easeOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute "
         >
-          {/*alternative object-contain*/}
+          {/*Profile Photo
+          alternative class object-contain*/}
           <Image
             priority
             src="/images/profile-red.png"
@@ -32,6 +33,36 @@ const Photo = () => {
             alt=""
           />
         </motion.div>
+
+        {/*circle absolute top-0 left-0 w-full h-full xl:w-[498px] xl:h-[498px]*/}
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          fill="transparent"
+          viewBow="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#A91717"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25 ", "16 25 92 72", "4 250 22 22"],
+              transition: {
+                delay: 1.5,
+                duration: 2.5,
+                ease: "easeInOut",
+                repeat: Infinity,
+                repeatType: "reverse",
+              },
+              rotate: [120, 360],
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
