0c4b8b662474c0c23901fb06982ca7e26360f4c3
Jason Alvarado <jayxalvarado@gmail.com>
2024-07-01 01:06:40 -0700

    s

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -40,12 +40,12 @@ const Stats = () => {
                   end={item.num}
                   duration={5}
                   delay={3}
-                  className="text-3xl xl:text-6xl font-extrabold"
+                  className="text-2xl xl:text-6xl font-extrabold"
                 />
                 <p
                   className={`${
                     item.text.length < 15 ? "max-w-[100px]" : "max-w[150px]"
-                  } leading-snug text-white/80`}
+                  } leading-snug text-white/80 text-sm xl:text-lg`}
                 >
                   {item.text}
                 </p>
