25839887976c3f5651e463a903f6d4e1aa110ede
Jason Alvarado <jayxalvarado@gmail.com>
2024-07-03 16:54:42 -0700

    cleanup helpful facts

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="flex flex-row justify-center align-center text-center">
-      <p className="m-10 p-auto h1 text-xl font-bold text ">
+    <section className="flex flex-row justify-center align-center text-center p-5 my-20 mx-auto h3 xl:h2">
+      <p className="">
         About me section currently in progress but this does not change the fact
         that in Australia, there are approximately 48 million kangaroos and in
         Uruguay, there are 3,457,380 living inhabitants.
         <br />
         <br />
-        So if the kangaroos decided to ever strike war and invade Uruguay, each
+        So if the kangaroos were to strike war and invade Uruguay, every
         Uruguayan will have to fight 14 kangaroos each.
       </p>
-    </div>
+    </section>
   );
 };
 
