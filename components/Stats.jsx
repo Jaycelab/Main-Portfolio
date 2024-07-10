a8070cfb8626289f2f2b453d5c3c3d192c0a9917
Jason Alvarado <jayxalvarado@gmail.com>
2024-07-10 14:05:51 -0700

    cleanup

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -3,23 +3,23 @@
 import CountUp from "react-countup";
 
 {
-  /*non-dynamic for now*/
+  /*non-dynamic for now. With Goal Stats */
 }
 const stats = [
   {
-    num: 18,
+    num: 20,
     text: "Technologies Learned",
   },
   {
-    num: 348,
+    num: 420,
     text: "Hours Coded",
   },
   {
-    num: 1110,
+    num: 1200,
     text: "Commits",
   },
   {
-    num: 11,
+    num: 12,
     text: "Projects Completed",
   },
 ];
@@ -28,11 +28,11 @@ const Stats = () => {
   return (
     <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
-        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto p-5  xl:max-w-none ">
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto  xl:max-w-none ">
           {stats.map((item, index) => {
             return (
               <div
-                className="flex-1 flex gap-4 items-center justify-center xl:justify-start bg-secondary/10 p-4 rounded-lg shadow-lg"
+                className="flex-1 flex gap-2 items-center justify-center xl:justify-start bg-secondary/30 p-2 rounded-lg shadow-lg xl:p-4 xl:gap-4 xl:rounded-xl xl:shadow-xl"
                 key={index}
               >
                 <CountUp
@@ -40,12 +40,12 @@ const Stats = () => {
                   end={item.num}
                   duration={5}
                   delay={3}
-                  className="text-2xl  xl:text-4xl font-extrabold "
+                  className="text-2xl xl:text-6xl font-extrabold "
                 />
                 <p
                   className={`${
-                    item.text.length < 15 ? "max-w-[150px]" : "max-w[250px]"
-                  } leading-tight text-white/80  sm:text-wrap sm:text-xs  xl:text-xl`}
+                    item.text.length < 15 ? "max-w-[150px]" : "max-w-[250px]"
+                  } leading-snug text-white/80 text-base`}
                 >
                   {item.text}
                 </p>
--- components/ui/Links.jsx
+++ components/ui/Links.jsx
@@ -8,7 +8,6 @@ const socials = [
   { icon: <FaGithub />, path: "https://www.github.com/Jaycelab" },
   { icon: <FaInstagram />, path: "https://www.instagram.com/Jaycelab" },
   { icon: <FaTwitter />, path: "https://www.twitter.com/Jaycelab" },
-  { icon: <FaShare />, path: "" },
 ];
 
 const Links = ({ containerStyles, iconStyles }) => {
