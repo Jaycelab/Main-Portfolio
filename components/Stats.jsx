50d94fc3b184f57ff40bbf58259ed8448ed1dc23
Jason Alvarado <jayxalvarado@gmail.com>
2024-07-10 15:05:55 -0700

    .

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -16,11 +16,11 @@ const stats = [
   },
   {
     num: 1200,
-    text: "Total Commits",
+    text: "Commits",
   },
   {
     num: 12,
-    text: "Projects Completed",
+    text: "Projects",
   },
 ];
 
@@ -32,7 +32,7 @@ const Stats = () => {
           {stats.map((item, index) => {
             return (
               <div
-                className="flex-1 flex gap-2 items-center justify-center xl:justify-start md:bg-secondary/30 p-2 rounded-lg shadow-lg xl:p-4 xl:gap-4 xl:rounded-xl xl:shadow-xl"
+                className="flex-1 flex gap-2 items-center justify-center xl:justify-start sm:bg-secondary/30 p-2 rounded-lg shadow-lg lg:p-4 lg:gap-4 xl:rounded-xl xl:shadow-xl"
                 key={index}
               >
                 <CountUp
@@ -40,12 +40,12 @@ const Stats = () => {
                   end={item.num}
                   duration={5}
                   delay={3}
-                  className="text-2xl md:text-4xl lg:text-4xl xl:text-6xl font-extrabold "
+                  className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold "
                 />
                 <p
                   className={`${
-                    item.text.length < 15 ? "max-w-[100px]" : "max-w-[200px]"
-                  } leading-snug text-white/80 text-sm`}
+                    item.text.length < 15 ? "max-w-[150px]" : "max-w-[300px]"
+                  } leading-tight text-white/80 text-xs `}
                 >
                   {item.text}
                 </p>
