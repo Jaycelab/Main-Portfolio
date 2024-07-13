2ede0ac3612d74fce1530cecd0e3af524b71ac47
Jason Alvarado <jayxalvarado@gmail.com>
2024-07-13 11:27:32 -0700

    stuff

--- components/ui/Links.jsx
+++ components/ui/Links.jsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
-import { FaGithub, FaInstagram, FaTwitter, FaGlobe } from "react-icons/fa";
+import {
+  FaGithub,
+  FaInstagram,
+  FaTwitter,
+  FaCameraRetro,
+} from "react-icons/fa";
 
 {
   /*social paths*/
 }
 const socials = [
+  { icon: <FaCameraRetro />, path: "https://www.pexels.com/@jaycelab" },
   { icon: <FaGithub />, path: "https://www.github.com/Jaycelab" },
   { icon: <FaInstagram />, path: "https://www.instagram.com/Jaycelab" },
   { icon: <FaTwitter />, path: "https://www.twitter.com/Jaycelab" },
-  { icon: <FaGlobe />, path: "/" },
 ];
 
 const Links = ({ containerStyles, iconStyles }) => {
