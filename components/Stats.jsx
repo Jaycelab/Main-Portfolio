b7563b8249bc5748477d15a6365410dad7854f61
jayxalvarado <jayxalvarado@gmail.com>
2024-07-14 12:03:56 -0700

    cleanup

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -11,40 +11,40 @@ const stats = [
     text: "Techs Learned",
   },
   {
-    num: 420,
+    num: 382,
     text: "Hours Coded",
   },
   {
-    num: 1200,
-    text: "Commits",
+    num: 1262,
+    text: " Total Commits",
   },
   {
-    num: 12,
-    text: "Projects",
+    num: 13,
+    text: "Projects Completed",
   },
 ];
 
 const Stats = () => {
   return (
-    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-6">
       <div className="container mx-auto">
         <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto  xl:max-w-none ">
           {stats.map((item, index) => {
             return (
               <div
-                className="flex-1 flex gap-2 items-center justify-center xl:justify-start sm:bg-secondary/30 p-0 sm:p-4 rounded-lg shadow-lg  lg:gap-4 xl:p-6 xl:rounded-xl xl:shadow-xl"
+                className="flex-1 flex gap-2 items-center justify-center xl:justify-start sm:bg-secondary/30 p-0 sm:p-4 rounded-lg shadow-lg  lg:gap-4  xl:rounded-xl xl:shadow-xl"
                 key={index}
               >
                 <CountUp
                   enableScrollSpy={true}
                   end={item.num}
                   duration={5}
-                  delay={3}
+                  delay={2}
                   className="text-2xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold "
                 />
                 <p
                   className={`${
-                    item.text.length < 15 ? "max-w-[150px]" : "max-w-[200px]"
+                    item.text.length < 20 ? "max-w-[150px]" : "max-w-[200px]"
                   } text-xs md:text-base leading-tight text-white/80 `}
                 >
                   {item.text}
